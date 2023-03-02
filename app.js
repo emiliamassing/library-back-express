84b7d8f09658e5a4c859cfeafb0ec819fd775e72
emiliamassing <emiliamassings@gmail.com>
2023-03-02 12:50:01 +0100

    added library router

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-const cors = requre('cors');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const libraryRouter = require('./routes/library');
 
 const app = express();
 
@@ -17,6 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/library', libraryRouter);
 
 module.exports = app;
